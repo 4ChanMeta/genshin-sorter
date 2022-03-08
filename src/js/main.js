cde76c4d686af9a218dd50075437f9ef8f42d672
fireworks88 <throwaway.546372819@protonmail.com>
2022-03-08 16:16:04 +0100

    dsjkd

--- src/js/main.js
+++ src/js/main.js
@@ -649,10 +649,10 @@ function setLatestDataset() {
 /** Populate option list. */
 function populateOptions() {
   const optList = document.querySelector('.options');
-  const optInsert = (name, id, tooltip, checked = true, disabled = false) => {
+  const optInsert = (name, id, tooltip, checked = false, disabled = false) => {
     return `<div><label title="${tooltip?tooltip:name}"><input id="cb-${id}" type="checkbox" ${checked?'checked':''} ${disabled?'disabled':''}> ${name}</label></div>`;
   };
-  const optInsertLarge = (name, id, tooltip, checked = true) => {
+  const optInsertLarge = (name, id, tooltip, checked = false) => {
     return `<div class="large option"><label title="${tooltip?tooltip:name}"><input id="cbgroup-${id}" type="checkbox" ${checked?'checked':''}> ${name}</label></div>`;
   };
 
