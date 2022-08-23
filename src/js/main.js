33a1276f67af9d9a8fb261f51a03eccf726874b8
4ChanMeta <56356893+4ChanMeta@users.noreply.github.com>
2022-08-22 20:43:42 -0400

    Update main.js
    Default being 3 is annoying so i made it 5

--- src/js/main.js
+++ src/js/main.js
@@ -484,7 +484,7 @@ function progressBar(indicator, percentage) {
  * 
  * @param {number} [imageNum=3] Number of images to display. Defaults to 3.
  */
-function result(imageNum = 3) {
+function result(imageNum = 5) {
   document.querySelectorAll('.finished.button').forEach(el => el.style.display = 'block');
   document.querySelector('.image.selector').style.display = 'block';
   document.querySelector('.time.taken').style.display = 'block';
